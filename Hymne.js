60ce053c5f8f7e793d65c54f2830105a735bbffb
AbelKhiew <khiewjy-wm25@student.tarc.edu.my>
2026-04-17 20:01:16 +0800

    Update Hymne.js

--- Hymne.js
+++ Hymne.js
@@ -1,37 +1,78 @@
 const hymns_en =[
   {
-    "id": 1,
-    "title": "Holy, Holy, Holy! Lord God Almighty",
-    "sections": [
+    id: 252,
+    title: "Pour Down on Me, Holy Spirit",
+    sections: [
       {
-        "type": "verse",
-        "content": [
-          { "cn": "Holy, holy, holy! Lord God Almighty!", "py": "Early in the morning our song shall rise to Thee;" },
-          { "cn": "Holy, holy, holy, merciful and mighty!", "py": "God in three Persons, blessed Trinity!" }
+        verse: 1,
+        content: [
+          { cn: "① Pour down on me, Holy Spirit, Fill my heart with Thy grace;" },
+          { cn: "Lord, keep me in Thy presence, Lest I turn from Thy face." }
         ]
       },
       {
-        "type": "verse",
-        "content": [
-          { "cn": "Holy, holy, holy! All the saints adore Thee,", "py": "Casting down their golden crowns around the glassy sea;" },
-          { "cn": "Cherubim and seraphim falling down before Thee,", "py": "Who was, and is, and evermore shall be." }
+        verse: 2,
+        content: [
+          { cn: "② Thou canst fill me, precious Spirit, Unto Thee will I bow;" },
+          { cn: "I need Thee to guide me, Come and dwell with me now." }
+        ]
+      },
+      {
+        verse: 3,
+        content: [
+          { cn: "③ I am weak, Lord, lend me strength now, Fill my heart with Thy love;" },
+          { cn: "Keep me on Thy pathway, With Thy pow’r from above." }
+        ]
+      },
+      {
+        verse: 4,
+        content: [
+          { cn: "④ May the stream of living water Fill my heart to the end," },
+          { cn: "Until in Thy kingdom, I will ne’er thirst again." }
         ]
       }
     ]
   },
   {
-    "id": 2,
-    "title": "Praise God, From Whom All Blessings Flow",
-    "sections": [
+    id: 242,
+    title: "Let Him In",
+    sections: [
       {
-        "type": "verse",
-        "content": [
-          { "cn": "Praise God, from whom all blessings flow;", "py": "" },
-          { "cn": "Praise Him, all creatures here below;", "py": "" },
-          { "cn": "Praise Him above, ye heavenly host;", "py": "" },
-          { "cn": "Praise Father, Son, and Holy Ghost. Amen.", "py": "" }
+        verse: 1,
+        content: [
+          { cn: "① There’s a Stranger at the door, Let Him in;" },
+          { cn: "He has been there oft before, Let Him in;" },
+          { cn: "Let Him in, ere He is gone, Let Him in, the Holy One," },
+          { cn: "Jesus Christ, the Father’s Son, Let Him in." }
+        ]
+      },
+      {
+        verse: 2,
+        content: [
+          { cn: "② Open now to Him your heart, Let Him in;" },
+          { cn: "If you wait He will depart, Let Him in;" },
+          { cn: "Let Him in, He is your Friend, He your soul will sure defend," },
+          { cn: "He will keep you to the end, Let Him in." }
+        ]
+      },
+      {
+        verse: 3,
+        content: [
+          { cn: "③ Hear you now His loving voice? Let Him in;" },
+          { cn: "Now, oh, now make Him your choice, Let Him in;" },
+          { cn: "He is standing at your door, Joy to you He will restore," },
+          { cn: "And His name you will adore, Let Him in." }
+        ]
+      },
+      {
+        verse: 4,
+        content: [
+          { cn: "④ Now admit the heav’nly Guest, Let Him in;" },
+          { cn: "He will make for you a feast, Let Him in;" },
+          { cn: "He will speak your sins forgiv’n, And when earth ties all are riv’n," },
+          { cn: "He will take you home to heav’n, Let Him in." }
         ]
       }
     ]
   }
-]
+];
